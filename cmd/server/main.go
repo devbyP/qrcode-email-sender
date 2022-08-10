e5a5609ba251a1b25093a57ee7bc818cf9d6c254
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-08-10 22:23:21 +0700

    exit when start fail

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,24 +10,41 @@ import (
 	"github.com/devbyP/qrcode-email-sender/pkgs/db"
 	"github.com/devbyP/qrcode-email-sender/pkgs/httpServer"
 	"github.com/devbyP/qrcode-email-sender/pkgs/userCode"
+
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load environment variables from .env file, if global env variables not exists.
+	godotenv.Load()
+
 	var dbCon string
 	var port int
 	var err error
 	var pdb *pgx.Conn
+
 	// set context timeout for seting up server.
 	setupCtx, cancel := context.WithTimeout(context.Background(), 20 * time.Second)
 	defer cancel()
+
 	// connect to 
 	dbCon = os.Getenv("DATABASE_URL")
 	if pdb, err = db.Connect(setupCtx, dbCon); err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
+
+	// database use uuid as unique id
+	err = db.GetUUIDLib(setupCtx, pdb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// migrate table to database.
-	userCode.CreateUserDBPgx(setupCtx, pdb, &userCode.User{}, &userCode.Ticket{})
+	err = userCode.CreateUserDBPgx(setupCtx, pdb, &userCode.User{}, &userCode.Ticket{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// create store db objects and assign to handlers.
 	// handler will have access to these db globally inside their package.
@@ -37,7 +54,7 @@ func main() {
 
 	// get port and set run server
 	if port, err = strconv.Atoi(os.Getenv("PORT")); err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 	httpServer.RunServer(port)
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/devbyP/qrcode-email-sender
 go 1.18
 
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/jackc/pgx/v5 v5.0.0-beta.1
+	github.com/joho/godotenv v1.4.0
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
