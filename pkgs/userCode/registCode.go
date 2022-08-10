e7d22feec2b714344ca59812522d6a284e435e98
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-08-10 18:30:59 +0700

    basic interface

--- pkgs/userCode/registCode.go
+++ pkgs/userCode/registCode.go
@@ -1,5 +1,13 @@
+// business logic
 package userCode
 
-type codeDB interface {
-	
+type CodeAdaptor interface {
+	RegisterUser(fName, lName, email, contact string) (string, error)
+	BuyTicket(uId, tname string) (string, error)
+	GetUsersTicket(email string) (*User, []*Ticket, error)
+}
+
+type CodeChecker interface {
+	GetUserTicketInfo(tId string) (*User, *Ticket, error)
+	CheckInTicket(tId string) error
 }
