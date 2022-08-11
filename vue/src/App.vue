2102ac7eee51c057f30c90c2f23eeec3651a893e
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-08-11 17:37:12 +0700

    header to component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="wrapper">
-    <header>
-      <h1>Get Email and QRcode!!</h1>
-      <div v-show="hasBack === true" @click="goback" class="back">&lt; go back</div>
-    </header>
+    <AppNavHeader />
     <main>
       <RouterView />
     </main>
@@ -14,17 +11,7 @@
 </template>
 
 <script setup lang="ts">
-import { RouterView } from "vue-router";
-import { storeToRefs } from "pinia";
-import { useStore } from "./store/back";
-import { useRouter } from "vue-router";
-
-const store = useStore();
-const router = useRouter();
-const { hasBack } = storeToRefs(store);
-function goback() {
-  router.push("/");
-}
+import AppNavHeader from "./components/AppNavHeader/AppNavHeader.vue";
 </script>
 
 <style scoped>
@@ -35,27 +22,6 @@ function goback() {
   align-items: center;
   height: 100vh;
   width: 100vw;
-}
-
-header {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #303030;
-  width: 100%;
-  height: 75px;
-}
-
-.back {
-  position: absolute;
-  left: 2rem;
-  transition: 0.4s;
-  background-color: transparent;
-  cursor: pointer;
-}
-.back:hover {
-  background-color: hsla(160, 100%, 37%, 1);
 }
 
 footer {
---
+++ vue/src/components/AppNavHeader/AppNavHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <header>
+    <h1>Get Email with QRcode!!</h1>
+    <div v-show="hasBack" @click="goback" class="back">&lt; Go Back</div>
+    <div v-show="toScan" @click="toScanPage" class="scanBtn">Scan Code</div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useHeaderStore } from "@/store/header";
+import { useRouter } from "vue-router";
+
+const store = useHeaderStore();
+const router = useRouter();
+const { hasBack, toScan } = storeToRefs(store);
+function goback() {
+  router.push("/");
+}
+function toScanPage() {
+  router.push("/scanner");
+}
+</script>
+
+<style scoped>
+header {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #303030;
+  width: 100%;
+  height: 75px;
+}
+.back {
+  position: absolute;
+  left: 2rem;
+  transition: 0.4s;
+  background-color: transparent;
+  cursor: pointer;
+}
+.back:hover {
+  background-color: hsla(160, 100%, 37%, 1);
+}
+.scanBtn {
+  position: absolute;
+  right: 2rem;
+  transition: 0.4s;
+  background-color: transparent;
+  cursor: pointer;
+}
+.scanBtn:hover {
+  background-color: hsla(160, 100%, 37%, 1);
+}
+</style>
